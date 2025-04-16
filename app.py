2c99e9e265fda215f66d1101fc0d208c23d4642e
kalam <nomattermoussa@gamil.com.com>
2025-04-16 23:43:24 +0100

    Fix port binding for Render

--- app.py
+++ app.py
@@ -100,5 +100,7 @@ def clean_data(df, normalize=False):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render va injecter la variable PORT
+    app.run(host="0.0.0.0", port=port)
+    #app.run(debug=True)
 
